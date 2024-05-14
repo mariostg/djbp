9655e75a785901b6b74b4a25d95bf18aab88563c
Mario St-Gelais <mario.stg@videotron.ca>
2024-05-14 00:24:19 -0400

    set debug for .env

--- main/settings.py
+++ main/settings.py
@@ -30,7 +30,7 @@ if "pytest" not in sys.modules:
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
