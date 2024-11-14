a736a1fe5132b0c8ab42740f84f7da47b25afc32
mariostg <mario.stg@videotron.ca>
2024-11-13 21:25:40 -0500

    Update models.py

--- project/models.py
+++ project/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Base(models.Model):
-    created = models.DateTimeField(auto_created=True)
+    created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     class Meta:
